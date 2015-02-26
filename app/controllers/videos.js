504cd4cf70298fe4e685cf4a2539e5da130f136d
ErikaLim <Erika.megan.lim@gmail.com>
2015-02-26 15:52:59 -0800

    moved video controller, video template and search to Videos

--- app/controllers/videos.js
+++ app/controllers/videos.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  videos: [],
+  actions: {
+    search: function() {
+
+      var _this = this;
+      var query = this.get('searchTerm');
+      var request = gapi.client.youtube.search.list({
+        q: query,
+        part: 'snippet'
+      });
+
+      request.execute(function(response) {
+        var videos = _this.get('videos')
+        var newVideos = []
+        for (var i = 0; i < response.items.length; i++) {
+          newVideos.push({
+            title: response.items[i].snippet.title,
+            url: response.items[i].snippet.thumbnails.default.url,
+            videoId: response.items[i].id.videoId,
+            description: response.items[i].snippet.description
+          });
+        }
+        _this.set("videos", newVideos)
+        // console.log(_this.get("videos"))
+        // console.log(response.items[0].snippet.thumbnails);
+      });
+    }
+  }
 });
