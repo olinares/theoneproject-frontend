3e2cc1e4257df53128d1c8f43c6e08bb1e8df5ca
ErikaLim <Erika.megan.lim@gmail.com>
2015-02-26 12:34:00 -0800

    fixed template

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
   videos: [],
   actions: {
     search: function() {
+
       var _this = this;
       var query = this.get('searchTerm');
       var request = gapi.client.youtube.search.list({
@@ -15,8 +16,8 @@ export default Ember.Controller.extend({
         var videos = _this.get('videos')
         var newVideos = [{title:"hello 2", description:"description3"}]
         _this.set("videos", newVideos)
-
-        console.log(response.items[0].snippet.description);
+        console.log(_this.get("videos"))
+        // console.log(response.items[0].snippet.description);
       });
     }
   }
