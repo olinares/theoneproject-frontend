8f69371906fd2f0322afe2ff08adce0444620229
nbacci <nmbachvarova@ucdavis.edu>
2015-02-27 12:13:51 -0800

    added heart icon and toggle property

--- app/controllers/videos.js
+++ app/controllers/videos.js
@@ -16,17 +16,21 @@ export default Ember.Controller.extend({
         var videos = _this.get('videos');
         var newVideos = [];
         for (var i = 0; i < response.items.length; i++) {
-          newVideos.push({
+          newVideos.push(Ember.Object.create({
             title: response.items[i].snippet.title,
             url: response.items[i].snippet.thumbnails.default.url,
             videoId: response.items[i].id.videoId,
             description: response.items[i].snippet.description
-          });
+          }));
         }
         _this.set("videos", newVideos);
         // console.log(_this.get("videos"))
         // console.log(response.items[0].snippet.thumbnails);
       });
+
+    },
+    favorite: function (video) {
+      video.toggleProperty('favorited');
     }
   }
 });
